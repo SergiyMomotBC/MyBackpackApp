4d34a9bf7b3bb2e0c404eac5c255ac891350b95f
Sergiy Momot <sergiy@sergiys-imac.t-mobile.com>
2017-02-14 22:25:53 -0500

    a

---
+++ MyBackpack/MenuController.swift
@@ -0,0 +1,105 @@
+//
+//  MenuController.swift
+//  My Backpack
+//
+//  Created by Sergiy Momot on 2/14/17.
+//  Copyright © 2017 Sergiy Momot. All rights reserved.
+//
+
+import UIKit
+
+class MenuController
+{
+    private let animationDuration = 0.12
+    private(set) var isShowing: Bool = false
+    
+    let menu: UIStackView!
+    let targetViewController: UIViewController!
+    let blurEffect: UIVisualEffectView?
+    
+    init(withStackView stackView: UIStackView, inViewController vc: UIViewController, useBlur shouldBlur: Bool) {
+        self.menu = stackView
+        self.targetViewController = vc
+        
+        if shouldBlur {
+            self.blurEffect = UIVisualEffectView(frame: self.targetViewController.view.frame)
+            self.setupBlurEffect()
+        } else {
+            self.blurEffect = nil
+        }
+        
+        self.setupMenu()
+    }
+    
+    func show(completion: @escaping (Bool) -> Void) {
+        UIView.animate(withDuration: self.animationDuration,
+                       delay: 0,
+                       options: UIViewAnimationOptions.curveLinear,
+                       animations:
+                            { () -> Void in
+                                self.menu.center.y = self.menu.bounds.height / 2 + 1
+                                self.blurEffect?.alpha = 1.0
+                            },
+                       completion: completion)
+        
+        self.isShowing = true
+    }
+    
+    func hide(completion: @escaping (Bool) -> Void) {
+        UIView.animate(withDuration: self.animationDuration,
+                       delay: 0,
+                       options: UIViewAnimationOptions.curveLinear,
+                       animations:
+                            { () -> Void in
+                                self.menu.center.y = -self.menu.bounds.height / 2
+                                self.blurEffect?.alpha = 0.0
+                            },
+                       completion: completion)
+        
+        self.isShowing = false
+    }
+    
+    private func setupMenu() {
+        self.targetViewController.view.addSubview(self.menu)
+        self.menu.center.y = -self.menu.bounds.height / 2
+        
+        let viewsDictionary = ["stackView": self.menu]
+        
+        let stackView_H = NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-0-[stackView]-0-|",
+            options: NSLayoutFormatOptions(rawValue: 0),
+            metrics: nil,
+            views: viewsDictionary)
+        
+        let stackView_V = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-0-[stackView(300)]|",
+            options: NSLayoutFormatOptions(rawValue:0),
+            metrics: nil,
+            views: viewsDictionary)
+        
+        self.targetViewController.view.addConstraints(stackView_V)
+        self.targetViewController.view.addConstraints(stackView_H)
+    }
+    
+    private func setupBlurEffect() {
+        self.blurEffect?.effect = UIBlurEffect(style: .light)
+        self.targetViewController.view.addSubview(self.blurEffect!)
+        self.blurEffect!.alpha = 0.0
+        let viewsDictionary = ["blurEffect": self.blurEffect]
+        
+        let horizontal = NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-0-[blurEffect]-0-|",
+            options: NSLayoutFormatOptions(rawValue: 0),
+            metrics: nil,
+            views: viewsDictionary)
+        
+        let vertical = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-0-[blurEffect]-0-|",
+            options: NSLayoutFormatOptions(rawValue: 0),
+            metrics: nil,
+            views: viewsDictionary)
+        
+        self.targetViewController.view.addConstraints(horizontal)
+        self.targetViewController.view.addConstraints(vertical)
+    }
+}
--- MyBackpack/ViewController.swift
+++ MyBackpack/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController
 {
     var pictureController: CameraController?
+    var menuController: MenuController!
     
     @IBOutlet weak var addCloseBarButton: UIBarButtonItem!
     @IBOutlet weak var blurEffect: UIVisualEffectView!
@@ -19,58 +20,27 @@ class ViewController: UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.pictureController = CameraController(forViewController: self)
+        //self.pictureController = CameraController(forViewController: self)
+        
+        self.menuController = MenuController(withStackView: self.menuView, inViewController: self, useBlur: true)
     }
 
     @IBAction func openCamera(_ sender: Any) {
         self.pictureController?.presentImagePicker()
     }
     
-    @IBAction func showMenu( _ sender: UIBarButtonItem) {
-        
-        if(self.menuView.superview == nil) {
-            self.view.addSubview(self.menuView)
-            self.menuView.center.y = -self.menuView.bounds.height / 2
-
-            let viewsDictionary = ["stackView": self.menuView]
-            
-            let stackView_H = NSLayoutConstraint.constraints(
-                withVisualFormat: "H:|-0-[stackView]-0-|",
-                options: NSLayoutFormatOptions(rawValue: 0),
-                metrics: nil,
-                views: viewsDictionary)
-            
-            let stackView_V = NSLayoutConstraint.constraints(
-                withVisualFormat: "V:|-0-[stackView(300)]|",
-                options: NSLayoutFormatOptions(rawValue:0),
-                metrics: nil,
-                views: viewsDictionary)
-            
-            view.addConstraints(stackView_H)
-            view.addConstraints(stackView_V)
-        }
-        
-        UIView.animate(withDuration: 0.12, delay: 0, options: UIViewAnimationOptions.curveLinear, animations: {
-            () -> Void in
-            if self.menuView.center.y < 0 {
-                self.menuView.center.y = self.menuView.bounds.height / 2 + 1
-                self.blurEffect.alpha = 1.0
-                
-            } else {
-                self.menuView.center.y = -self.menuView.bounds.height / 2
-                self.blurEffect.alpha = 0.0
-            }
-        }, completion: { success in
-            if self.menuView.center.y < 0 {
-                self.menuView.removeFromSuperview()
-                let barButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(self.showMenu))
-                self.navigationController?.navigationBar.topItem?.rightBarButtonItem = barButton
-            } else {
+    @IBAction func showMenu(_ sender: UIBarButtonItem) {
+        if !self.menuController.isShowing {
+            self.menuController.show(completion: { success in
                 let barButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.stop, target: self, action: #selector(self.showMenu))
                 self.navigationController?.navigationBar.topItem?.rightBarButtonItem = barButton
-            }
-        })
-
+            })
+        } else {
+            self.menuController.hide(completion: { success in
+                let barButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(self.showMenu))
+                self.navigationController?.navigationBar.topItem?.rightBarButtonItem = barButton
+            })
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -79,6 +49,4 @@ class ViewController: UIViewController
             vc.sourceImage = self.pictureController?.takenImage ?? nil
         }
     }
-
 }
-
